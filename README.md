6445418b00974b4e42ab8feb6e61ce373d3ce22e
Rob Allen <rob@akrabat.com>
2014-12-15 09:47:57 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,75 @@
+# RKA Slim Controller
+
+An extension to [Slim][1] that allows you use to dynamically instantiated
+controllers with action methods wherever you would use a currently callable.
+
+The controller can optionally be loaded from Slim's DI container, allowing you
+to inject dependencies as required.
+
+
+## Installation
+
+    composer require rka/rka-slim-controller
+
+[1]: http://www.slimframework.com/
+
+
+## Usage
+
+Use the string format `{controller class name}:{action method name}` wherever you
+would usually use a closure:
+
+e.g.
+
+    $app = new \RkaSc\Slim();
+    $app->get('/hello:name', 'App\Controller\IndexController:homeAction');
+
+
+You can also register the controller with Slim's DI container:
+
+    $app = new \RkaSc\Slim();
+    $app->container->singleton('App\IndexController', function ($container) {
+        // Retrieve any required dependencies from the container and
+        // inject into the constructor of the controller
+
+        return new \App\Controller\IndexController();
+    });
+
+    $app->get('/', 'App\IndexController:indexAction');
+
+
+## Controller class methods
+
+*RKA Slim Controller* will call the controller's `setApp()`, `setRequest()` and
+`setResponse()` methods if they exist and populate appropriately.
+
+Hence, a typical controller may look like:
+
+    <?php
+    namespace App\Controller;
+
+    class IndexController
+    {
+        protected $request;
+        protected $response;
+
+        public function indexAction()
+        {
+            echo "This is the home page";
+        }
+
+        public function helloAction($name)
+        {
+            echo "Hello, $name";
+        }
+
+        public function setRequest($request)
+        {
+            $this->request = $request;
+        }
+
+        public function setResponse($response)
+        {
+            $this->response = $response;
+        }
+    }
---
+++ RkaSc/Slim.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Override of \Slim\Slim to add the ability to dynamically create a controller
+ * and call an action method on it.
+ *
+ * Copyright 2014 Rob Allen (rob@akrabat.com).
+ * License: New-BSD
+ */
+namespace RkaSc;
+
+class Slim extends \Slim\Slim
+{
+    /**
+     * Add a route as per the parent method, additionally supporting the syntax
+     * "{controller class name}:{action method name}" as the last argument which
+     * will be converted to a closure that instantiates the controller (or gets
+     * from container) and then calls the method on it.
+     *
+     * @inheritdoc
+     *
+     * @param   array (See notes above)
+     * @return  \Slim\Route
+     */
+    public function mapRoute($args)
+    {
+        $callable = array_pop($args);
+        if (is_string($callable) && strpos($callable, ':')) {
+            $callable = $this->createControllerClosure($callable);
+        }
+        $args[] = $callable;
+
+        return parent::mapRoute($args);
+    }
+
+    /**
+     * Create a closure that instantiates (or gets from container) and then calls
+     * the action method.
+     *
+     * Also if the methods exist on the controller class, call setApp(), setRequest()
+     * and setResponse() passing in the appropriate object.
+     *
+     * @param  string $name controller class name and action method name separated by a colon
+     * @return closure
+     */
+    protected function createControllerClosure($name)
+    {
+        list($controllerName, $actionName) = explode(':', $name);
+
+        // Create a callable that will find or create the controller instance
+        // and then execute the action
+        $app = $this;
+        $callable = function () use ($app, $controllerName, $actionName) {
+
+            // Try to fetch the controller instance from Slim's container
+            if ($app->container->has($controllerName)) {
+                $controller = $app->container->get($controllerName);
+            } else {
+                // not in container, assume it can be directly instantiated
+                $controller = new $controllerName($app);
+            }
+
+            // Set the App, request and response into the controller if we can
+            if (method_exists($controller, 'setApp')) {
+                $controller->setApp($app);
+            }
+            if (method_exists($controller, 'setRequest')) {
+                $controller->setRequest($app->request);
+            }
+            if (method_exists($controller, 'setResponse')) {
+                $controller->setResponse($app->response);
+            }
+
+            return call_user_func_array(array($controller, $actionName), func_get_args());
+        };
+
+        return $callable;
+    }
+}
